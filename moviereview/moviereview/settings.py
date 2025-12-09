7465e756d4dc5fc6c8bee07bdd08461918d4d1f3
HARISHMALISETTY <malisettyharish@gmail.com>
2025-12-09 22:18:50 +0530

    changes

--- moviereview/moviereview/settings.py
+++ moviereview/moviereview/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # loads .env file
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l5g5vwzwg^e$l1=y!#@)*%@e0mszxbo23hl62ro=je*@eahgv5'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
 
 # ALLOWED_HOSTS = ["django-level-one.onrender.com"] #prod mode
-ALLOWED_HOSTS=[] #dev mode
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+
 
 
 # Application definition
@@ -79,11 +85,11 @@ WSGI_APPLICATION = 'moviereview.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': "movie_reviews",
-        'USER':'root',
-        'PASSWORD':'Harish258@@',
-        'HOST':"127.0.0.1",
-        'PORT':"3306",
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST':os.getenv("DB_HOST"),
+        'PORT':os.getenv("DB_PORT"),
         "OPTIONS":{
             'charset':'utf8mb4',
             'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
